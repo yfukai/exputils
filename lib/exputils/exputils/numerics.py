fd793247cb8783344be5b5db9d86e36bb63a3edd
Yosuke Fukai <fukai.yosuke@gmail.com>
2014-11-26 14:52:26 +0900

    autocommit

--- lib/exputils/exputils/numerics.py
+++ lib/exputils/exputils/numerics.py
@@ -31,6 +31,51 @@ def get_log_separated_array(begin, end, count):
 def fmod_positive(a,b):
     return np.fmod(a,b) + (a<0)*b 
 
+def calc_linear_fitting_consts(xs,ys):
+    mean_x = np.mean(xs)
+    mean_x_times_2 = np.mean(xs**2)
+    mean_y = np.mean(ys)
+    var_x = mean_x_times_2-mean_x**2
+    covar = np.mean(xs*ys) - mean_x*mean_y
+    a = var_x / covar
+    b = mean_y - a * mean_x
+    return a, b
 
-def linear_fitting(xs,ys,xmin=None,xmax=None):
-    return a,da,b,db
+def linear_fitting(xs,ys,dxs=None,dys=None,xmin=None,xmax=None):
+    xs = np.array(xs); ys = np.array(ys)
+    indices = np.where(np.logical_and(xs >= xmin,  xs <= xmax))
+    xs = xs[indices] ; ys = ys[indices]
+    dxs = dxs[indices] ; dys = dys[indices]
+    a,b = calc_linear_fitting_consts(xs,ys)
+    
+    if dxs == None:
+        dxs = np.zeros(len(indices))
+    if dys == None:
+        dys = ys - a * xs - b
+        std = np.std(dys)
+        dys = np.array([std]*len(indices))
+
+    das = np.empty([len(indices),2])
+    dbs = np.empty([len(indices),2])
+
+    for i in indices:
+        xs2 = np.copy(xs)
+        xs3 = np.copy(xs)
+        xs2[i] = xs[i]+dxs[i]
+        xs3[i] = xs[i]-dxs[i]
+        a2, b2 = calc_linear_fitting_consts(xs2,ys)
+        a3, b3 = calc_linear_fitting_consts(xs2,ys)
+        das[i,0] = max(abs(a2-a),abs(a3-a))
+        dbs[i,0] = max(abs(b2-b),abs(b3-b))
+        ys2 = np.copy(ys)
+        ys3 = np.copy(ys)
+        ys2[i] = ys[i]+dys[i]
+        ys3[i] = ys[i]-dys[i]
+        a2, b2 = calc_linear_fitting_consts(xs,ys2)
+        a3, b3 = calc_linear_fitting_consts(xs,ys3)
+        das[i,1] = max(abs(a2-a),abs(a3-a))
+        dbs[i,1] = max(abs(b2-b),abs(b3-b))
+
+     da = np.sqrt(np.sum(np.flatten(das)**2))
+     db = np.sqrt(np.sum(np.flatten(dbs)**2))
+     return a,da,b,db
