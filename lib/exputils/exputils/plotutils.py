547c666e1532f90ce3132a627691e17f3652a8b4
Yosuke Fukai <fukai.yosuke@gmail.com>
2015-08-11 16:30:49 +0900

    autocommit

--- lib/exputils/exputils/plotutils.py
+++ lib/exputils/exputils/plotutils.py
@@ -1,4 +1,22 @@
 import matplotlib.pyplot as plt
+import seaborn
+
+def plt_guideline(b,e,slope,label="",style="-b",left=False,ha="right",va = "top",plot_dist=plt):
+    if len(b) == 2 and len(e) == 1:
+        bx = b[0]
+        by = b[1]
+        ex = e[0]
+        ey = by+((ex-bx)*slope)
+    elif len(b) == 1 and len(e) == 2:
+        bx = b[0]
+        ex = e[0]
+        ey = e[1]
+        by = ey-((ex-bx)*slope)
+    plot_dist.plot([bx,ex],[by,ey],style)
+    x = bx if left else ex
+    y = by if left else ey
+    plt_dist.text(x,y,label,ha=ha,va=va)
+
 
 def plt_guideline_log(b,e,exponent,label="",style="-b",left=False,ha="right",va = "top",plot_dist=plt):
     if len(b) == 2 and len(e) == 1:
@@ -11,13 +29,15 @@ def plt_guideline_log(b,e,exponent,label="",style="-b",left=False,ha="right",va 
         ex = e[0]
         ey = e[1]
         by = ey/((ex/bx)**exponent)
-    plt.loglog([bx,ex],[by,ey],style)
+    plt_dist.loglog([bx,ex],[by,ey],style)
     x = bx if left else ex
     y = by if left else ey
-    plt.text(x,y,label,ha=ha,va=va)
+    plt_dist.text(x,y,label,ha=ha,va=va)
 
-def plt_line(bx,y,ex,label="",style="--",left=False,ha="right",va = "top",plot_dist=plt):
-    plt.plot([bx,ex],[y,y],style)
+def plt_horizontal_line(xs,y,label="",style="--",left=False,ha="right",va = "top",plot_dist=plt):
+    bx = xs[0]
+    ex = xs[1]
+    plt_dist.plot([bx,ex],[y,y],style)
     x = bx if left else ex
-    plt.text(x,y,label,horizontalalignment=ha,va=va)
+    plt_dist.text(x,y,label,horizontalalignment=ha,va=va)
 
