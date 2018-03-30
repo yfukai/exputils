07f43bfff0bbbf49db9f982aa0a7cdfaad452250
Yosuke Fukai <fukai.yosuke@gmail.com>
2018-03-30 14:34:55 +0900

    added color wheel

--- lib/exputils/plotutils/__init__.py
+++ lib/exputils/plotutils/__init__.py
@@ -3,6 +3,28 @@ import matplotlib.pyplot as plt
 from matplotlib import ticker
 from . import cm
 
+#https://stackoverflow.com/questions/31940285/plot-a-polar-color-wheel-based-on-a-colormap-using-python-matplotlib
+def color_wheel(cmap,fig=plt.figure()):
+    display_axes = fig.add_axes([0.1,0.1,0.8,0.8], projection='polar')
+#    display_axes._direction = 2*np.pi ## This is a nasty hack - using the hidden field to 
+                                      ## multiply the values such that 1 become 2*pi
+                                      ## this field is supposed to take values 1 or -1 only!!
+    
+    norm = mpl.colors.Normalize(0.0, 2*np.pi)
+    
+    # Plot the colorbar onto the polar axis
+    # note - use orientation horizontal so that the gradient goes around
+    # the wheel rather than centre out
+    quant_steps = 2056
+    cb = mpl.colorbar.ColorbarBase(display_axes, cmap=cmap,
+                                       norm=norm,
+                                       orientation='horizontal')
+    
+    # aesthetics - get rid of border and axis labels                                   
+    cb.outline.set_visible(False)                                 
+    display_axes.set_axis_off()
+    plt.show() # Replace with plt.savefig if you want to save a file
+
 def legend_reverse(ax=None,**kwargs):
     if ax is None: ax=plt.gca()
     handles, labels = ax.get_legend_handles_labels()
